125f548fa08e6ef600065b9c9ab146d7a1f51676
Tomas Bisi <tommybisi@gmail.com>
2017-12-31 14:42:20 -0800

    started new pyhton project. setting up virtualenv and dependecies

---
+++ messageApp.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+from flask_socketio import SocketIO, emit
+
+app = Flask(__name__)
+
+app.config['SECRET KEY'] = 'testasd123'
+socketio = SocketIO(app)
+
+@app.route('/')
+def index():
+  return render_template('./index.html')
+
+if __name__ == '__main__':
+  socketio.run(app, debug = True)
+
+
